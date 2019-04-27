a62def1cf04f3c49de4f542e3a520653d70a4335
Yangfan Ren <42377778+renyangfan960902@users.noreply.github.com>
2019-04-27 14:49:20 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,20 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+from setuptools import setup
+
+setup(
+    name='hLDA-nCRP',
+    url='https://github.com/renyangfan960902/STA663-Project-hLDA.git',
+    author='Jiayi Ding, Yangfan Ren, Chudi Zhong',
+    author_email='yangfan.ren@duke.edu',
+    # Needed to actually package something
+    packages=['hLDA-nCRP'],
+    version='0.1',
+    license='MIT',
+    description='An example of a python package from pre-existing code',
+)
+
